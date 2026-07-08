0f674b0e757464c8bc0f3248e7f035ea5c9da715
Sasadism <104597434+Sasadism@users.noreply.github.com>
2026-07-08 22:36:48 +0330

    Initialize Fastify server with basic route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
+import Fastify from "fastify";
 
+const app = Fastify({
+  logger: true,
+});
+
+app.get("/", async () => {
+  return {
+    name: "Zeus-NG",
+    version: "1.0.0",
+    status: "Running 🚀",
+  };
+});
+
+const start = async () => {
+  try {
+    await app.listen({
+      host: "0.0.0.0",
+      port: Number(process.env.PORT) || 3000,
+    });
+
+    console.log("Zeus-NG Started");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
